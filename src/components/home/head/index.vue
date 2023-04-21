fb68000a234ccb2dff6f2b7c9048e6cbf95efdb8
ZWenchang <your-email-address>
2023-04-21 20:51:24 +0800

    改变导航栏

--- src/components/home/head/index.vue
+++ src/components/home/head/index.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="header">
-        <ul>
-            <div class="menu">
-                <li class="logo">
-                    <img src="src/assets/zzz.png">
-                </li>
-                <div class="right">
-                    <li><router-link to="/" class="link">blog</router-link></li>
-                    <li><router-link to="/blog" class="link">about</router-link></li>
-                </div>             
+        <div class="menu">
+            <div class="logo">
+                <input id="mychackbox" type="checkbox">
+                <div class="left">
+                    <label for="mychackbox" id="show"><img src="src/assets/zzz.png"></label>
+                </div>
+                <div class="hide">
+                    <ul>
+                        <router-link to="/" class="link"><li style="margin-top: 50px;">blog</li></router-link>
+                        <router-link to="/blog" class="link"><li>about</li></router-link>
+                    </ul>
+                </div>
             </div>
-        </ul>
+        </div>
     </div>
 </template>
 <script setup lang = 'ts'>
@@ -20,11 +23,12 @@ import { ref, reactive } from 'vue'
 <style scoped lang="less">
 a {
     text-decoration: none;
+    color: aliceblue;
 }
 
-ul {
-    height: 50px;
-}
+// ul {
+//     height: 50px;
+// }
 
 #header {
     background-color: rgb(225, 89, 15);
@@ -32,48 +36,98 @@ ul {
     height: 50px;
 }
 
-#header .right {
-    display: flex;
-    // margin-left: 1200px;
-    float: right;
-
+.logo {
+    height: 50px;
 }
 
-#header .menu {
-    display: flex;
-    /* display: flex：弹性盒子，很适合多个元素按一行或者一列排开 */
-    height: 100%;
-    /* 设置和导航栏 header 相同的高度 */
-    line-height: 50px;
-    /* 设置行高，注意不要使用 inherit（继承），因为 menu 的父标签不是 header，而是 ul，我们并没有设置 ul 的行高，无法继承 */
-    /* 最大宽度 */
-    padding: 0 20px;
-    justify-content:space-between;
-}
+// #header .right {
+//     display: flex;
+//     // margin-left: 1200px;
+//     float: right;
 
-#header .menu li {
-    height: 100%;
-    /* 设置高度，让每一个标签的高度也等于 header 高度 */
-}
+// }
+
+// #header .menu {
+//     display: flex;
+//     /* display: flex：弹性盒子，很适合多个元素按一行或者一列排开 */
+//     height: 100%;
+//     /* 设置和导航栏 header 相同的高度 */
+//     line-height: 50px;
+//     /* 设置行高，注意不要使用 inherit（继承），因为 menu 的父标签不是 header，而是 ul，我们并没有设置 ul 的行高，无法继承 */
+//     /* 最大宽度 */
+//     padding: 0 20px;
+//     justify-content: space-between;
+// }
+
+// #header .menu li {
+//     height: 100%;
+//     /* 设置高度，让每一个标签的高度也等于 header 高度 */
+// }
 
 #header li:hover {
-    background: rgb(241, 99, 16);
+    background: rgb(241, 150, 97);
     transition: 0.5s;
     /* 持续 0.5 秒 */
+
 }
 
-#header .menu li .link {
+li {
     font: bolder normal 16px Source Sans Pro;
-    color: #fff;
     /* 不解释，看不懂的补 CSS 知识去 */
     padding: 14px 15px;
     font-size: medium;
+    text-decoration: none;
 }
 
 img {
     margin-top: 5px;
     height: 40px;
     width: 40px;
-    margin-left: 20px;
+    position:absolute;
 }
-</style>
+
+// label #home {
+//     display: none;
+// }
+
+// label #blog {
+//     display: none;
+// }
+#mychackbox {
+    display: none;
+}
+
+// #mychackbox:checked~.hide {
+//     display: block;
+//     width: 100px;
+// }
+
+#mychackbox:checked~.left {
+    position: fixed;
+    left: 80px;
+    transition: all .5s ease;
+}
+
+#mychackbox:checked~.hide {
+    left: 0;
+    transition: all .5s ease;
+}
+
+.hide {
+    position: fixed;
+    left: -80px;
+    width: 80px;
+    height: 100%;
+    background: rgb(225, 89, 15);
+    transition: all .5s ease;
+}
+.left{
+    position: fixed;
+    left: 10px;
+    width: 80px;
+    transition: all .5s ease;
+}
+
+label {
+    cursor: pointer;
+}</style>
